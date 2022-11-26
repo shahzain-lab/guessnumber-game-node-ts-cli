3d423643472f53d1153d6810ebee341db0248e3f
shahzain-lab <shahzain30196@gmail.com>
2022-11-26 21:55:44 +0500

    up to ts 4.9+ and npm config setup

--- build/index.js
+++ build/index.js
@@ -1,34 +1,33 @@
+#!/usr/bin/env node
+
 // let's build number guessing game !!!
 import chalk from 'chalk';
 import figlet from 'figlet';
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
-
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-
-function startGame(): void {
+function startGame() {
     inquirer.prompt([
-    {
-        type: 'input',
-        name: 'result',
-        message: chalk.bgBlue(' 🎁 Enter number between 1 to 100')
-    }
-    ]).then((ans: { result: string}) => {
+        {
+            type: 'input',
+            name: 'result',
+            message: chalk.bgBlue(' 🎁 Enter number between 1 to 100')
+        }
+    ]).then((ans) => {
         console.log(chalk.yellow('________________'));
         const guess = Number(ans.result);
-        if(!isNaN(guess)) {
-            if(guess < randomNumber) {
-            console.log(chalk.magenta(`Your guess is too low ${chalk.yellow(guess)}`));
-            console.log(chalk.yellow('________________'));
+        if (!isNaN(guess)) {
+            if (guess < randomNumber) {
+                console.log(chalk.magenta(`Your guess is too low ${chalk.yellow(guess)}`));
+                console.log(chalk.yellow('________________'));
                 startGame();
             }
-            if(guess > randomNumber) {
+            if (guess > randomNumber) {
                 console.log(chalk.magenta(`Your guess is too high ${chalk.yellow(guess)}`));
                 console.log(chalk.yellow('________________'));
                 startGame();
             }
-            if(guess === randomNumber) {
+            if (guess === randomNumber) {
                 console.log(chalk.green(` 🥳 🥳 Congrat. Your guess ${chalk.yellow(guess)} is correct 🎉 🎉 `));
                 console.log(chalk.yellow('________________'));
                 inquirer.prompt([
@@ -38,22 +37,20 @@ function startGame(): void {
                         message: chalk.bgCyan('Do you wanna start again ? 🔥 ')
                     }
                 ]).then(ans => {
-                    if(ans.continue) {
+                    if (ans.continue) {
                         randomNumber = Math.floor(Math.random() * 100) + 1;
                         startGame();
                     }
-                })
+                });
             }
-        }else {
+        }
+        else {
             console.log(chalk.red(' 🌚 🌚  please avoid typing string....'));
             console.log(chalk.yellow('________________'));
             startGame();
         }
-        
-    }).catch((err: string) => console.log(err))
+    }).catch((err) => console.log(err));
 }
-
-
 figlet.text('ts-guess-me', {
     horizontalLayout: 'default',
     verticalLayout: 'default',
